052ad9fac878ea4c511b3d301b34c72e554d9595
Tommy Kärnström <tommy@karnstrom.com>
2026-02-03 23:12:13 +0100

    feat: sort searchbox item by name and display favourite next to name

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -16,11 +16,14 @@ import itemJSON from "@/data/items.json";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { createImageUrlPath } from "@/playground/items/utils/image";
+import { useFavouriteItems } from "@/store/favourite-items";
+import { StarIcon } from "lucide-react";
 
-const items = itemJSON as Item[];
+const items = itemJSON.sort((a, b) => a.name.localeCompare(b.name)) as Item[];
 
 export function SearchBox() {
   const router = useRouter();
+  const favouriteItems = useFavouriteItems((state) => state.items);
 
   const handleAddItem = (item: Item | null) => {
     if (item === null) return;
@@ -42,7 +45,11 @@ export function SearchBox() {
         <ComboboxEmpty>No items found.</ComboboxEmpty>
         <ComboboxList>
           {(item) => (
-            <ComboboxItem key={item.id} value={item} className="py-1">
+            <ComboboxItem
+              key={item.id}
+              value={item}
+              className="cursor-pointer py-1"
+            >
               <div className="flex flex-row gap-2 items-center">
                 <Image
                   src={createImageUrlPath(item.image)}
@@ -50,7 +57,13 @@ export function SearchBox() {
                   width={28}
                   height={28}
                 />
-                <span>{item.name}</span>
+                <div className="grow">{item.name}</div>
+
+                {favouriteItems.includes(item.id) ? (
+                  <StarIcon className="w-4 h-4 text-yellow-400 fill-yellow-400 ml-auto" />
+                ) : (
+                  <StarIcon className="w-4 h-4 text-neutral-400 ml-auto" />
+                )}
               </div>
             </ComboboxItem>
           )}
