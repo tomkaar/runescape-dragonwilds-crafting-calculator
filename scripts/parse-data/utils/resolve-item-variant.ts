94806d5eb4d032078afb536cdccc27602eebc5b8
Tommy Kärnström <tommy@karnstrom.com>
2026-02-15 19:32:05 +0100

    feat: include image property in resolveItemVariant function

--- scripts/parse-data/utils/resolve-item-variant.ts
+++ scripts/parse-data/utils/resolve-item-variant.ts
@@ -6,6 +6,7 @@ export function resolveItemVariant(item: SourceItem): ItemVariant | null {
   return {
     id: idFromName(item.item_name),
     name: item.item_name,
+    image: item.json.image_raw || null,
     variantName: null,
     recipe: null,
   };
