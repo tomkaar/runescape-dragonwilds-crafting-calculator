d215ca49dfb352d2ff0ffe52d7324b2022dd8620
Tommy Kärnström <tommy@karnstrom.com>
2026-03-15 20:43:09 +0100

    feat: replace BookSearch icon with ChevronRight in RecipeCard component

--- components/Items/AllRecipes/components/Card.tsx
+++ components/Items/AllRecipes/components/Card.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Item } from "@/Types";
 import { createImageUrlPath } from "@/scripts/parse-data/utils/image-url";
-import { BookSearch, ChevronRight } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 
 type TodoRecipeCardProps = {
   item: Item | null;
@@ -25,7 +25,7 @@ export function Card(props: TodoRecipeCardProps) {
           <div className="text-base">
             <div className="flex flex-row gap-2 items-center">
               {item?.name} ({materials.length})
-              <BookSearch size={16} />
+              <ChevronRight size={16} />
             </div>
             {multiplier > 1 && (
               <div className="text-xs text-title -mt-0.5">
