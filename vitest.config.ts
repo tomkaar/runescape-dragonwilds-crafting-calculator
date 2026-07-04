f01397b5bddfedae8b46f74bda7813649f392b9b
Tommy Kärnström <tommy@karnstrom.com>
2026-07-04 10:56:08 +0200

    chore: update testing configuration and add coverage support

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8'
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
